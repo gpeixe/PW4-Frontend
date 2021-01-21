5556a74dbea9894660167be25030f71794b84d86
Thiago <thiago.nascimento99@hotmail.com>
2021-01-21 00:37:12 -0300

    Created Home Searchbar

--- src/Pages/HomePage/Components/SearchBar.js
+++ src/Pages/HomePage/Components/SearchBar.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import { makeStyles, withTheme } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import { FormControl, IconButton, InputAdornment, OutlinedInput } from '@material-ui/core';
+import { useHistory } from 'react-router';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: '50ch',
     },
-  },
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
 }));
+
+
 
 export default function SearchBar() {
   const classes = useStyles();
-
+  const history = useHistory();
+  function searchForSummoner(name){
+    history.push(`/Summoner/${name}`);
+  }
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    </form>
+    <form>
+    <FormControl className={classes.root}>
+      <OutlinedInput id="outlined-adornment-weight" placeholder="Summoner's Name"
+      endAdornment={
+        <InputAdornment position="end">
+          <IconButton
+            aria-label="Search"
+            edge="end"
+            onClick={(props) => console.log(props)}
+          >
+          <SearchIcon />
+          </IconButton>
+        </InputAdornment>
+      } 
+      />
+      </FormControl>
+      </form>
   );
 }
