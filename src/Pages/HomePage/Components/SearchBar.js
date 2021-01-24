1116cc06411b72bd401c806cdd07b29116daa3db
Thiago <thiago.nascimento99@hotmail.com>
2021-01-24 18:49:01 -0300

    Search Bar working

--- src/Pages/HomePage/Components/SearchBar.js
+++ src/Pages/HomePage/Components/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
 import { FormControl, IconButton, InputAdornment, OutlinedInput } from '@material-ui/core';
@@ -16,31 +16,39 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-
-
 export default function SearchBar() {
   const classes = useStyles();
   const history = useHistory();
+  // declaração do estado do valor da SearchBar e uma função que altera esse state
+  const [ searchState, setState ] = useState();
   function searchForSummoner(name){
-    history.push(`/Summoner/${name}`);
+    if(name) {
+      history.push(`/Summoner/${name}`);
+      console.log(name)
+    }
   }
+
   return (
-    <form>
     <FormControl className={classes.root}>
-      <OutlinedInput id="outlined-adornment-weight" placeholder="Summoner's Name"
+      <OutlinedInput id="outlined-adornment-weight" placeholder="Summoner's Name" autoFocus={true} 
       endAdornment={
         <InputAdornment position="end">
           <IconButton
             aria-label="Search"
             edge="end"
-            onClick={(props) => console.log(props)}
+            onClick={() => searchForSummoner(searchState)} // searchState vai estar atualizado com o último valor digitado
           >
           <SearchIcon />
           </IconButton>
         </InputAdornment>
-      } 
+      }
+      onChange={(e) => setState(e.target.value)} // atualiza o searchState
+      onKeyDown={(e) => {
+        if(e.key === 'Enter'){
+          searchForSummoner(searchState)
+        }
+      }}
       />
       </FormControl>
-      </form>
   );
 }
